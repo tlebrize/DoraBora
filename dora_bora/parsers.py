474a3a3d1ab24db976a0ca16acda7132c1d9ea9f
taek <lebrize.t@gmail.com>
2023-03-11 12:55:01 +0100

    wip

--- dora_bora/parsers.py
+++ dora_bora/parsers.py
@@ -17,3 +17,23 @@ def decode_cell_b64(encoded):
 
 def encode_cell_b64(decoded):
     return BASE64[decoded // 64] + BASE64[decoded % 64]
+
+
+def get_int_by_hashed_value(c):
+    for i, a in enumerate(BASE64):
+        if a == c:
+            return i
+    return -1
+
+
+def int_to_byte(value):
+    return int.to_bytes(value)
+    # return [bool((value >> i) & 1) for i in range(8)]
+
+
+def decode_map_data(data):
+    for chunk in [data[0 + i : 10 + i] for i in range(0, len(data), 10)]:
+        cells = [int_to_byte(get_int_by_hashed_value(c)) for c in chunk]
+        print(cells[2])
+        walkable = (cells[2] & 56) >> 3
+        print(walkable)
